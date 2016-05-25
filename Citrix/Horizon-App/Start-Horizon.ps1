e7031d45db6145978f58a5ff405ec19acc57aed5
Justin King <justin.king@agile360.com>
2016-05-25 12:41:13 -0700

    Launch Horizon software for client

---
+++ Citrix/Horizon-App/Start-Horizon.ps1
@@ -0,0 +1,66 @@
+#These variables should be updated as needed for the environment 
+$Horizon = "legaclt.exe"
+$HorizonPath = "C:\Users\Public\Rocket Software\LegaSuite Windows Client\7.2456.1.34"
+$HorizonArguements = '/h10.118.32.30 /GWorD="'+$HorizonPath+'\Production" /M2924 /GPanR=1 /GFonR=1 /GDisPR=1 /GCCSID=37 /GShowEmulator=1 /GMAIWS=59 /Gdevns=off'
+$CSVFile = "Workstation-Mapping.csv"
+$CSVFilePath = ".\"
+[switch]$CreateCSVIfMissing = $false
+$WSTemplate = "WS-Template.ws"
+$WSTemplatePath = ".\"
+$TempPath = $env:TEMP
+[switch]$DoNotCleanupTemp = $false
+
+#Import the CSV File and store variables in $csvdata
+$FileName= $CSVFilePath+"\"+$CSVFile
+If(!(Test-Path -Path ($FileName)) -and ($CreateCSVIfMissing)) {
+    try {
+        New-Item ($FileName) -type file -force -ErrorAction STOP | Out-Null
+        $NewLine = "WorkstationName,DJNumber,PNumber"
+        $NewLine | add-content -path $FileName -ErrorAction STOP
+        }
+    catch {
+            $E = $_.Exception.GetBaseException()
+            write-error $E.ErrorInformation.Description -ErrorAction Stop
+            }
+    } #End If
+elseif (!(Test-Path -Path ($FileName))) {
+    Write-Error "$FileName not found.  Please update the script." -ErrorAction Stop
+    }
+$CSVData = (import-csv $FileName | where-object {$_.WorkstationName -eq $env:computername})[0] #<-- if more than one is found, only grab the first one!
+
+#Rebuild the Horizon launch Arguements adding the DJNumber then launch the program
+$FileName = $HorizonPath+"\"+$Horizon
+$HorizonArguements = $HorizonArguements+" /Gdevn="+$CSVData.DJNumber
+try {
+    Start-Process -FilePath $FileName -ArgumentList $HorizonArguements -ErrorAction Stop
+    }
+catch {
+     $E = $_.Exception.GetBaseException()
+     write-error $E.ErrorInformation.Description -ErrorAction Stop   
+    }
+
+#If the PNumber is defined, create the ws template and launch
+If ($CSVData.PNumber) {
+    try {
+        #Generate the random filename
+        $Rset=$Null
+        $WSFileName=$Null
+        $set = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray()
+        for ($i=1; $i -le 6; $i++) {
+            $Rset += $set | Get-Random
+        }
+        $WSFileName = $TempPath+"\"+$Rset + ".ws"
+        $SourceName = $WSTemplatePath+"\"+$WSTemplate
+        (Get-Content $SourceName).replace('[DJNumber]',$CSVData.DJNumber).replace('[PNumber]',$CSVData.PNumber) | Set-Content $WSFileName -force -ErrorAction Stop
+        Start-Process -FilePath $WSFileName -ErrorAction Stop
+        }
+    catch {
+        $E = $_.Exception.GetBaseException()
+        write-error $E.ErrorInformation.Description -ErrorAction Stop 
+        }
+    } #End Take PNumber Action
+
+#Cleanup the WSFileName file if it exists
+If ($WSFileName -and !($DoNotCleanupTemp)) {
+    Remove-Item -Path $WSFileName -force
+    }
---
+++ Citrix/Horizon-App/WS-Template.ws
@@ -0,0 +1,43 @@
+[Profile]
+ID=WS
+Version=9
+[CT]
+trace=Y
+[Telnet5250]
+HostName=10.118.32.30
+AssociatedPrinterStartMinimized=N
+AssociatedPrinterClose=N
+AssociatedPrinterTimeout=0
+Security=CA400
+SSLClientAuthentication=Y
+CertSelection=AUTOSELECT
+[KeepAlive]
+KeepAliveTimeOut=0
+[Communication]
+Link=telnet5250
+Session=5250
+AutoConnect=N
+[5250]
+SessionType=Printer
+HostCodePage=037-U
+WorkStationID=[DJNumber]
+PrinterType=IBM3812
+[Keyboard]
+CuaKeyboard=2
+Language=United-States
+IBMDefaultKeyboard=N
+DefaultKeyboard=C:\Users\cshare\AppData\Roaming\IBM\Client Access\Emulator\private\AS400.KMP
+[PrintSessionSetup5250]
+Wallpaper=C:\Program Files (x86)\IBM\Client Access\Emulator\printsessionsetup.bmp
+[LastExitView]
+A=3 56 50 896 492 3 11 14 400 0 IBM3270— 37
+[printers]
+printer=Client/[PNumber]#/Generic / Text Only,winspool,Client\[PNumber]#\Generic / Text Only
+CPI=10
+LPI=6
+FaceName=[Device Font 10cpi]
+Drawer1Orient=Portrait
+Drawer2Orient=Portrait
+VTPrintArea=Scroll
+VTPrintChar=ASCII
+VTTerminator=None
