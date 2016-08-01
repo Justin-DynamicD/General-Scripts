a5716107e78588608e310a65de7781188784a7da
Justin King <justin.king@agile360.com>
2016-07-31 21:42:50 -0700

    Major Update

--- Exchange/MigrationModule.psm1
+++ Exchange/MigrationModule.psm1
@@ -36,6 +36,7 @@ function Initialize-O365User
 
     #Variables specific to client
     $groupList = "Office 365 Enrollment", "Office 365 Enterprise Cal"
+    $onlineSMTP = "viacom.mail.onmicrosoft.com"
 
     #Load AD modules
     If (!(Get-module ActiveDirectory)) {
@@ -52,6 +53,23 @@ function Initialize-O365User
             } #End Try
         Catch {write-error "Cannot import MSOL modules, please make sure they are avialable" -ErrorAction "Stop"}
         }
+
+    #Get CurrentUser and needed SMTP values
+    $currentUser = get-aduser -filter {name -eq $UserName}
+    $currentMailbox = get-mailbox $UserName
+    $newProxy = "smtp:"+$UserName+"@"+$onlineSMTP
+
+    #Check UPN to primary address
+    IF ($currentUser.UserPrincipalName -ne $currentMailbox.primarysmtpaddress) {
+         Write-Warning "the UPN and primary SMTP do not match.  Please correct"
+    }
+    
+    #Check for Proxy Address, add if missing
+    
+    IF ($currentUser.proxyAddresses -notcontains $newProxy) {
+        Write-Verbose "Adding address $newProxy"
+        set-ADUser $UserName -add proxyAddresses = $newProxy
+    }
 
     #Check each user to be a member of the groups
     $members=@()
@@ -75,23 +93,37 @@ function Initialize-O365User
 function Move-O365User {
 
     param (
-       # UserName this is the name of the account you wish to prepare for migration
+        # UserName this is the name of the account you wish to prepare for migration
         [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][Alias('user')] 
         [String]$UserName,
 
-        # Credentials These are the credentials require to sign into your O365 tenant
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][Alias('creds')]
-        [PsCredential]$Credentials
+        # RemoteHostName These are valid endpoints for replicating to the cloud
+        [Parameter(Mandatory=$false)][ValidateSet("owa.viacom.com","owa.mtvne.com","mail.paramount.com")][Alias('RHN')] 
+        [String]$RemoteHostName = "owa.viacom.com",
+
+        # OnlineCredentials These are the credentials require to sign into your O365 tenant
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][Alias('ocreds')]
+        [PsCredential]$OnlineCredentials,
+
+        # LocalCredentials These are the credentials require to sign into your Exchange Environment
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][Alias('lcreds')]
+        [PsCredential]$LocalCredentials
     )
 
     #Connect to the Exchange online environment and clobber all modules
     If (!(Get-module mSOLSession)) {
         Try {
-            $mSOLSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Credentials -Authentication Basic -AllowRedirection
+            $mSOLSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $OnlineCredentials -Authentication Basic -AllowRedirection
             $importResults = Import-PSSession $mSOLSession -AllowClobber
             Write-Verbose $importResults
             } #End Try
         Catch {write-error "Cannot import MSOL modules, please make sure they are avialable" -ErrorAction "Stop"}
         }  
+    
+    #Variables specific to client
+    $targetDeliveryDomain = "viacom.mail.onmicrosoft.com"
+
+    # Do the move
+    New-MoveRequest -Identity $UserName -Remote -RemoteHostName $RemoteHostName -RemoteCredential $LocalCredentials -TargetDeliveryDomain $targetDeliveryDomain -BadItemLimit 100 -AcceptLargeDataLoss
 
 } #End Function
